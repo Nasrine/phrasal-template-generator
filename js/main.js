8ad598e3ce1347ee42215ce60ca6afd4f1c6d9c9
Nasrine <azar0006@algonquinlive.com>
2016-03-03 14:07:05 -0500

    some js codes to start

---
+++ js/main.js
@@ -0,0 +1,24 @@
+var A colour = 'orange'
+colours = [Orange, indigo, etc.];
+  i = 0,
+
+var A creature = 'dragon'
+  creatures = [dragon, moth, etc.];
+  j= 0,
+
+var An adjective = 'super'
+  [beautiful, super, etc.];
+  k = 0,
+
+var A past tense verb = 'fell'
+  [ran, burped, etc.];
+  l = 0,
+
+var answer = 'A'
+  ['A', 'B', 'C'];
+
+if  (a == 'Snacking')
+  console.log(story);
+
+b) Escaping
+c)Winning
