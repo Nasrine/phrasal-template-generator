224c6775eaf5a869baaaafba39833eddfdbb08f2
Nasrine <azar0006@algonquinlive.com>
2016-03-03 21:58:01 -0500

    just to test it

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
-var A colour = 'orange'
-colours = [Orange, indigo, etc.];
-  i = 0,
 
-var A creature = 'dragon'
+
+var newName = prompt('Enter a letter');
+
+var i;
+
+for (i = 0; i < 10; i++) {
+  document.write(newName + '<br>');
+}
+
+/*var newColour = prompt('Enter a color');
+  var i;
+
+  for (i = 0; i < 1; i++) {
+  document.write(newColour + '<br>');
+}
+
+/*var A creature = 'dragon'
   creatures = [dragon, moth, etc.];
   j= 0,
 
@@ -16,6 +29,25 @@ var A past tense verb = 'fell'
 
 var answer = 'A'
   ['A', 'B', 'C'];
+
+var planet = 'Venus';
+
+// Compare the variable planet to the string ‘Venus’
+if (planet == 'Venus') {
+  // If planet is equal to ‘Venus’ then execute this code
+}
+var planet = 'Earth',
+  hasMoon = true
+;
+
+// Since the variable is true, we don’t have to compare against true
+// The if-statement is always checking for a truthy scenario
+if (hasMoon) {
+  // Execute this code if the scenario is truthy
+} else {
+  // Execute this code if the scenario is falsey
+  // The else leaves us a catch all if the first scenario is falsey
+}
 
 if  (a == 'Snacking')
   console.log(story);
