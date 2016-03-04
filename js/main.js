9ae5dc8db5bb2ee8ce63fb7004f2e08bd547709e
Nasrine <azar0006@algonquinlive.com>
2016-03-04 08:46:56 -0500

    if statements put

--- js/main.js
+++ js/main.js
@@ -2,46 +2,28 @@ var newStory = alert('Let’s make a story together!');
 
 var colour = prompt('A colour (orange, indigo, etc.)');
 
-{
-  document.write(colour + '<br>');
-}
 
 var creature = prompt('A creature (dragon, moth, etc.)');
 
-{
-  document.write(creature + '<br>');
-}
 
 var adjective = prompt('An adjective (beautiful, super, etc.)');
 
-{
-  document.write(adjective + '<br>');
-}
 
 var pastTense = prompt('A past tense verb (ran, burped, etc.)');
 
-{
-  document.write(pastTense + '<br>');
-}
 
 var story = prompt('Which story would you like?\n[a] Snacking\n[b] Escaping\n[c] Winning\nEnter the letter below.');
 
-var abc = ['a', 'b', 'c'];
-
-{
-a=function () {
-document.write('After snacking on' + adjective + 'treats, the' + colour + 'bellied' + creature + verb + 'for hours.');
-};
-
-a=function () {
-document.write('Ameilia' + verbed + 'through the' + adjective + colour + 'nebula escaping the space' + creature.);
-};
-
-c= function (c)
-document.write('Jackson chose his' +adjective,+ colour+ creature +'card and' + verbed +'it to the table knowing he won.');
-};
-
-
-if (story== 'a' || story == 'A'){
-  ..a()
+if (story == 'a') {
+ document.write('After snacking on' + adjective + 'treats, the' + colour + 'bellied' + creature + verb + 'for hours.');
 }
+
+  if (story == 'b') {
+ document.write('Ameilia' + verbed + 'through the' + adjective + colour + 'nebula escaping the space' + 'creature'.);
+}
+
+if (story == 'c') {
+  document.write('Jackson chose his' + adjective + colour+ 'creature' + 'card and' + verb + 'it to the table knowing he won'.);
+}
+
+
